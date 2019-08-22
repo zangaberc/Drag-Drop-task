2cd0ea87f081d1a20217abe7d5febc8cb9383768
Žan Gaberc <37248532+zangaberc@users.noreply.github.com>
2019-08-22 22:09:26 +0200

    Add files via upload

---
+++ src/components/Board.vue
@@ -0,0 +1,27 @@
+<template>
+  <div
+    :id="id"
+    class="board"
+    @dragover.prevent
+    @drop.prevent="Drop"
+  >
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['id'],
+  methods: {
+    Drop: e => {
+      const card_id = e.dataTransfer.getData('card_id');
+
+      const card = document.getElementById(card_id);
+
+      card.style.display = "block";
+
+      e.target.appendChild(card);
+    }
+  }
+}
+</script>
